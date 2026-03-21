04f7300f28b3beaaf48039a00faaa946255d1f7e
chauhanmeet456-code <chauhanmeet456@gmail.com>
2026-03-21 15:56:18 +0530

    Set index page

--- Login-Page.js
+++ Login-Page.js
@@ -1,31 +1,72 @@
-/* =========================
-   PASSWORD EYE TOGGLE
-========================= */
-const passwordInput = document.getElementById("password");
-const eyeIcon = document.getElementById("togglePassword");
+document.addEventListener("DOMContentLoaded", function(){
 
-eyeIcon.addEventListener("click", () => {
-  if (passwordInput.type === "password") {
-    passwordInput.type = "text";
-    eyeIcon.textContent = "🙈";
-  } else {
-    passwordInput.type = "password";
-    eyeIcon.textContent = "👁";
+  /* =========================
+     PASSWORD TOGGLE
+  ========================= */
+  const passwordInput = document.getElementById("password");
+  const eyeIcon = document.getElementById("togglePassword");
+
+  if(eyeIcon && passwordInput){
+    eyeIcon.addEventListener("click", () => {
+      passwordInput.type =
+        passwordInput.type === "password" ? "text" : "password";
+
+      eyeIcon.textContent =
+        passwordInput.type === "password" ? "👁" : "🙈";
+    });
   }
-});
 
 
-/* =========================
-   DARK / LIGHT MODE
-========================= */
-const themeBtn = document.getElementById("themeToggle");
+  /* =========================
+     DARK / LIGHT MODE
+  ========================= */
+  const themeBtn = document.getElementById("themeToggle");
 
-themeBtn.addEventListener("click", () => {
-  document.body.classList.toggle("light");
+  if(themeBtn){
+    themeBtn.addEventListener("click", () => {
+      document.body.classList.toggle("light");
 
-  if (document.body.classList.contains("light")) {
-    themeBtn.textContent = "☀️";
-  } else {
-    themeBtn.textContent = "🌙";
+      themeBtn.textContent =
+        document.body.classList.contains("light") ? "☀️" : "🌙";
+    });
   }
+
+
+  /* =========================
+     MULTIPLE USERS LOGIN
+  ========================= */
+  const users = [
+    { username: "admin", password: "123" },
+    { username: "ajay", password: "111" },
+    { username: "brijesh", password: "222" }
+  ];
+
+  const loginForm = document.getElementById("loginForm");
+
+  if(loginForm){
+    loginForm.addEventListener("submit", function(e){
+      e.preventDefault();
+
+      const username = document.getElementById("username").value.trim();
+      const password = document.getElementById("password").value.trim();
+
+      // check user
+      const validUser = users.find(user =>
+        user.username === username && user.password === password
+      );
+
+      if(validUser){
+
+        // SAVE LOGIN
+        localStorage.setItem("user", username);
+
+        // REDIRECT
+        window.location.href = "home.html";
+
+      } else {
+        alert("Wrong Username or Password ❌");
+      }
+    });
+  }
+
 });
