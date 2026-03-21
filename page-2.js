1b107fb94728f1ccdda3a73724efaa09d695a47b
chauhanmeet456-code <chauhanmeet456@gmail.com>
2026-03-21 15:51:33 +0530

    Added home and page 2

---
+++ page-2.js
@@ -0,0 +1,20 @@
+/* CHECK LOGIN (PROTECT PAGE) */
+const user = localStorage.getItem("user");
+
+if(!user){
+  // જો direct page open કરે → પાછું login
+  window.location.href = "sem-2.html";
+} else {
+  // username show
+  const text = document.getElementById("welcomeText");
+  if(text){
+    text.innerText = "Welcome " + user;
+  }
+}
+
+
+/* LOGOUT */
+function logout(){
+  localStorage.removeItem("user");
+  window.location.href = "home.html";
+}
