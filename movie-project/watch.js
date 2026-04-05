91fe6d9f81a2823cf7208547ad5175b30a3a5cd9
chauhanmeet456-code <chauhanmeet456@gmail.com>
2026-04-05 11:15:25 +0530

    3 idots movies fix

--- movie-project/watch.js
+++ movie-project/watch.js
@@ -23,7 +23,7 @@ const movies = {
   },
   "3idiots": {
     title: "3 Idiots",
-    youtube: "https://www.youtube.com/watch?v=K0eDlFX9GMc?autoplay=1&rel=0"
+    youtube: "https://www.youtube.com/embed/K0eDlFX9GMc?autoplay=1&rel=0"
   },
   "kgf": {
     title: "KGF",
